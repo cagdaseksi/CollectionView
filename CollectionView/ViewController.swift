b7b5be9aa5c75f7a9e39d11d857c23ae828d7c71
cagdaseksi <cagdas.eksi@hotmail.com>
2018-12-24 01:13:48 +0300

    initial commit

--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    
+    let array:[String] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let itemSize = UIScreen.main.bounds.width/3 - 2
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: itemSize, height: itemSize)
+        
+        layout.minimumInteritemSpacing = 2
+        layout.minimumLineSpacing = 2
+        
+        myCollectionView.collectionViewLayout = layout
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //Number of views
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    //Populate view
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! myCell
+        cell.imageView.image = UIImage(named: array[indexPath.row] + ".jpg")
+        return cell
+    }
+    
 }
-
---
+++ CollectionView/myCell.swift
@@ -0,0 +1,15 @@
+//
+//  myCell.swift
+//  CollectionView
+//
+//  Created by MAC on 24/12/18.
+//  Copyright © 2018 cagdaseksi. All rights reserved.
+//
+
+import UIKit
+
+class myCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
